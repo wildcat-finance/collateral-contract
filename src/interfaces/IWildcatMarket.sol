dcd6c52d310a8802b771c714fc3c86e9a9134746
d1ll0n <dillon@wildcat.finance>
2025-04-29 10:36:15 -0500

    update interface

--- src/interfaces/IWildcatMarket.sol
+++ src/interfaces/IWildcatMarket.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.20;
 
-import "v2-protocol/src/libraries/MarketState.sol";
+import {MarketState} from "v2-protocol/libraries/MarketState.sol";
 
 interface IWildcatMarket {
     function asset() external view returns (address);
